424336eef2829b160781affefd6a6e005c926f8c
ameganvic2-rgb <ameganvic2@gmail.com>
2026-09-16 19:53:02 +0000

    Update produits.js
    amélioration 1

--- produits.js
+++ produits.js
@@ -1,12 +1,22 @@
+
 const produits = [
-  { id: 1, nom: "Résistance 220Ω", prix: 100, categorie: "résistance", image: "" },
-  { id: 2, nom: "Résistance 1kΩ", prix: 100, categorie: "résistance", image: "" },
-  { id: 3, nom: "LED rouge 5mm", prix: 150, categorie: "LED", image: "" },
-  { id: 4, nom: "LED verte 5mm", prix: 150, categorie: "LED", image: "" },
-  { id: 5, nom: "Condensateur 100µF", prix: 200, categorie: "condensateur", image: "" },
-  { id: 6, nom: "Transistor NPN 2N2222", prix: 300, categorie: "transistor", image: "" },
-  { id: 7, nom: "Arduino Uno", prix: 8500, categorie: "carte électronique", image: "" },
-  { id: 8, nom: "Breadboard 830 points", prix: 2000, categorie: "accessoire", image: "" },
-  { id: 9, nom: "Fils de connexion (lot de 40)", prix: 1000, categorie: "accessoire", image: "" },
-  { id: 10, nom: "Buzzer 5V", prix: 250, categorie: "capteur", image: "" },
-];
+  { id: 1, nom: "Résistance 220Ω", prix: 100, categorie: "résistance", image: "images/resistance.png" },
+  { id: 2, nom: "Résistance 1kΩ", prix: 100, categorie: "résistance", image: " " },
+  { id: 3, nom: "Pack de 16 LED rouge jaune verte bleue ", prix: 2500, categorie: "Kit", image:" " },
+  
+  { id: 4, nom: "Condensateur 100µF", prix: 200, categorie: "condensateur", image: " " },
+  { id: 5, nom: "Transistor NPN 2N2222", prix: 300, categorie: "transistor", image: " " },
+  { id: 6, nom: "Arduino Uno", prix: 8500, categorie: "carte électronique", image: " " },
+  { id: 7, nom: "Breadboard 830 points", prix: 2000, categorie: "accessoire", image: " "  },
+  { id: 8, nom: "Fils de connexion mâle-mâle femmelle-femmelle (lot de 40)", prix: 1000, categorie: "accessoire", image: " " },
+  { id: 9, nom: "Buzzer 5V", prix: 250, categorie: "capteur", image: " "},
+  { id: 10, nom: "Mini kit arduino pour projets basique", prix: 10000, categorie: "Kit", image: " " },
+  { id: 11, nom: "Kit arduino complet", prix: 18000, categorie: "Kit", image: " " },
+  { id: 12, nom: "Kit pour detecteur de niveau d'eau", prix: 15000, categorie: "Kit", image: " " },
+  { id: 13, nom: "Kit pour feu tricolore", prix: 10000, categorie: "Kit", image: " " },
+  { id: 14, nom: "Mini drone avec camera", prix: 15000, categorie: "complement", image: " " },
+  
+
+
+
+]
